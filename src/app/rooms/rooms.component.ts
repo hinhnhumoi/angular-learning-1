c4b526e337c7d1edf6436f995702bf242da86805
hinhnhumoi <thanhtung28011997@gmail.com>
2024-08-05 14:11:00 +0700

    abc

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.scss'
+})
+export class RoomsComponent {
+
+  hotelName = 'Hilton Hotel';
+
+  numberOfRooms = 10;
+
+  hideRooms = false;
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+}
